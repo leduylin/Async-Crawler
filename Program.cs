39554500f01ccdf58fcd2e2766c3f7350a9bf051
leduylin <lin.le.set18@eiu.edu.vn>
2022-05-17 10:36:23 +0700

    done using async await for crawling, 2s per house solution

--- Program.cs
+++ Program.cs
@@ -1,74 +1,207 @@
-﻿using HtmlAgilityPack;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
-using System.Net.Http;
+using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace AsyncCrawler
+namespace UrlHouses
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            var a = DateTime.Now.Ticks;
+
+            List<string> listUrl = new List<string>();
+            IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver(Environment.CurrentDirectory);
+            driver.Manage().Window.Minimize();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
+
             try
             {
-                Stopwatch stopwatch = new Stopwatch();
-                stopwatch.Start();
+                driver.Url = "https://nha.chotot.com/mua-ban-bat-dong-san";
 
-                var html = new HtmlWeb();
-                string url = "https://nha.chotot.com/mua-ban-nha-dat-thanh-pho-thu-dau-mot-binh-duong";
-                var document = html.Load(url);
-                int count = 0;
-                var houses = document.DocumentNode.SelectNodes("//div[@class='list-view']/div/div/ul/div/li");
-                crawlData(houses);
-                stopwatch.Stop();
-                Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+                int totalOfPages = 1;
+                while (true)
+                {
+                    driver.Url = String.Format("https://nha.chotot.com/mua-ban-bat-dong-san?page={0}", totalOfPages);
+                    bool existPage = driver.PageSource.Contains("NotFound_notFoundWrapper__1Erzd");
+                    if (!existPage)
+                    {
+                        new WebDriverWait(driver, new TimeSpan(0, 0, 1500)).Until(c => c.FindElement(By.ClassName("list-view")));
+                        IList<IWebElement> aTags = driver.FindElements(By.XPath("//div[@class='list-view']/div/div/ul/div/li/a"));
+                        int ccountHouse = 0;
+                        foreach (IWebElement element in aTags)
+                        {
+                            string urlHouse = element.GetAttribute("href");
+                            if (urlHouse != null)
+                            {
+                                listUrl.Add(urlHouse);
+                            }
+                            ccountHouse++;
+                            if (ccountHouse > 10)
+                            {
+                                break;
+                            }
+                        }
 
+                        totalOfPages++;
 
-                //string uri = "https://nha.chotot.com/mua-ban-nha-dat-thanh-pho-thu-dau-mot-binh-duong";
-                //HttpClient http = new HttpClient();
-                //http.BaseAddress = new Uri(uri);
-                //var res = http.GetStringAsync("").Result;
-                //Console.WriteLine(res);
+                        if (totalOfPages > 2)
+                        {
+                            break;
+                        }
+                    }
+                }
+                driver.Close();
+                driver = new ChromeDriver();
+                driver.Dispose();
             }
-            catch (HttpRequestException e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            aaa(listUrl);
+
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine(m);
+
+
+
         }
-
-        public static void crawlData(HtmlNodeCollection houses)
+        private static async Task aaa(List<string> listUrl)
         {
-
-            foreach (var house in houses)
+            var a = DateTime.Now.Ticks;
+            int dem = 0;
+            List<Task<String>> awaitingTasks = new List<Task<string>>();
+            for (int i = 0; i < listUrl.Count(); ++i)
             {
-                Thread currentThreat = new Thread(()=> getRealEstate(house));
-                currentThreat.Start();
-                //Thread.Sleep(100);
+                dem++;
+                var task = GetUrl(listUrl[i], dem);
+
+                awaitingTasks.Add(task);
+
+                await Task.WhenAny(awaitingTasks);
+
             }
+            await Task.WhenAll(awaitingTasks);
         }
-        public static void getRealEstate (HtmlNode house)
+        private static async Task<string> GetUrl(string url, int dem)
         {
-            var node = house.SelectSingleNode(".//a");
-
-            if (node != null)
+            Console.WriteLine("Running: " + url);
+            return await Task.Run(() =>
             {
-                string uri = "https://nha.chotot.com" + node.Attributes["href"].Value;
+                getInfor(url, dem);
 
-                //html of a house link
-                HttpClient http = new HttpClient();
-                http.BaseAddress = new Uri(uri);
-                var res =  http.GetStringAsync("").Result;
+                return "Done";
+            });
 
-                var title = Regex.Match(res, @"(<h1 class=\""AdDecription_adTitle__2I0VE\"" [\b\D]*>)([\s].*?[\s]*)(?=<\/h1>)", RegexOptions.Singleline).Value.Replace("<h1 class=\"AdDecription_adTitle__2I0VE\" itemProp=\"name\"> <!-- -->", "");
-                var acreage = Regex.Match(res, @"(\""AdParam_adParamValue__1ayWO\""\>)(.*?)(?=<\/span>)", RegexOptions.Singleline).Value.Replace("\"AdParam_adParamValue__1ayWO\">", "");
-                var price = Regex.Match(res, @"(<span class=\""AdDecription_price__O6z15\"">)[\w\D]*(?=<span class=\""AdDecription_squareMetre__2KYh8\"">)", RegexOptions.Singleline).Value.Replace("<span class=\"AdDecription_price__O6z15\"><span itemProp=\"price\">", "");
+        }
+        private static void getInfor(string urlHouse, int dem)
+        {
+            try
+            {
+                IWebDriver childDriver = new OpenQA.Selenium.Chrome.ChromeDriver(Environment.CurrentDirectory);
+                childDriver.Manage().Window.Minimize();
+                childDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(1000);
+
+                try
+                {
+                    childDriver.Url = urlHouse;
+                    dem++;
+                    //get house's information
+                    var title = childDriver.FindElement(By.CssSelector(".AdDecription_adDecriptionWrapper__36qgN>.AdDecription_adTitle__2I0VE")).Text;
+                    var area = childDriver.FindElement(By.CssSelector(".ct-detail .AdDecription_adDecriptionWrapper__36qgN .AdDecription_squareMetre__2KYh8")).Text;
+                    var address = childDriver.FindElement(By.CssSelector(".ct-detail .AdParam_address__1pmCR>span")).Text;
+                    address = Regex.Match(address, @"[\w\D]*(?=.{10})", RegexOptions.Singleline).Value;
+                    var price = childDriver.FindElement(By.CssSelector(".ct-detail .AdDecription_adDecriptionWrapper__36qgN .AdDecription_price__O6z15>span")).Text;
+                    price = Regex.Match(price, @"[0-9]*\s.*(?=\-)", RegexOptions.Singleline).Value;
+                    var telephone = childDriver.FindElement(By.CssSelector("div.LeadButtonSlide_wrapperLeadButton__2Ytpy")).GetAttribute("innerHTML");
+                    telephone = Regex.Match(telephone.ToString(), @"(?<=\""tel\:)[\d]*(?=\"">)", RegexOptions.Singleline).Value;
+                    var description = childDriver.FindElement(By.CssSelector(".ct-detail  .AdDecription_adDecriptionWrapper__36qgN .AdDecription_adBody__1c8SG")).Text;
+
+                    //get house's image
+                    var btnImg = childDriver.FindElements(By.CssSelector(".AdImage_adImageWrapper__15vl2 .slick-dots>li")).Count;
+                    List<string> imgs = new List<string>();
+                    while (btnImg > 0)
+                    {
+                        IWebElement btn = childDriver.FindElement(By.CssSelector(".AdImage_adImageWrapper__15vl2 .slick-slider .AdImage_Next__26U9L"));
+                        btn.Click();
+                        btnImg--;
+
+                        var imgTags = childDriver.FindElement(By.CssSelector(".ct-detail .AdImage_sliderImage__3REqQ .AdImage_imageWrapper__1yIxE")).GetAttribute("innerHTML");
+                        imgTags = Regex.Match(imgTags, @"(?<=src=\"")https:\/\/cdn\.chotot\.com(.*)\.jpg(?=\"")", RegexOptions.Singleline).Value;
+                        imgs.Add(imgTags);
+                    }
+
+                    Console.WriteLine("\n\n" + title + "\n" + address + "\n" + area + "\n" + price + "\n" + telephone + "\n" + description + "\n\n");
+
+                    childDriver.Close();
+                    Thread.Sleep(2000);
+                    childDriver.Dispose();
+
+                    Console.WriteLine(dem);
+
+                }
+                catch (Exception ec)
+                {
+                    Console.WriteLine("Child driver " + ec.Message);
+                }
+            }
+            catch (Exception e)
+            {
+
             }
         }
     }
 }
+
+
+
+
+//var a = DateTime.Now.Ticks;
+//
+//String[] urls = new string[] { "a", "b" };
+//for (int i = 0; i < urls.Length; ++i)
+//{
+//    var task = GetUrl(urls[i]);
+
+//    awaitingTasks.Add(task);
+//    if (awaitingTasks.Count == 2)
+//    {
+//        await Task.WhenAny(awaitingTasks);
+
+
+//    }
+
+//}
+//await Task.WhenAll(awaitingTasks);
+//Console.WriteLine(DateTime.Now.Ticks - a);
+
+//static async Task<string> GetUrl(string url)
+//{
+//    Console.WriteLine("Running: " + url);
+//    return await Task.Run(() =>
+//    {
+//        long i = (long)2e8;
+
+//        while (i-- > 0)
+//        {
+//            if (i % 100000000 == 0)
+//            {
+//                Console.WriteLine(url + " " + i);
+//            }
+//        }
+//        return "a";
+//    });
+
+//}
